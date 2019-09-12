832e2f1c8520d6397192f331da0083742a39dee6
Brent Mercer <hicx76@hotmail.com>
2019-09-12 08:44:35 -0600

    Added admin dashboard and pie chart

--- admin/index.php
+++ admin/index.php
@@ -20,84 +20,108 @@
                 <div class="col-lg-12">
                     <h1 class="page-header">
                         Dashboard
-                        <small>Subheading</small>
                     </h1>
-
-                    <?php
-
-                    // $photo = Photo::find_by_id(7);
-                    // echo $photo->filename;
-
-                    // Testing create with associative array
-                    // $user = new User();
-                    // $user->username = "grod555";
-                    // $user->password = "334455";
-                    // $user->first_name = "Gavin";
-                    // $user->last_name = "Rosdale";
-                    // $user->create();
-
-                    // Testing update with associative array
-                    // $user = User::find_user_by_id(10);
-                    // $user->username = "crunk567";
-                    // $user->password = "popo90";
-                    // $user->first_name = "Frank";
-                    // $user->last_name = "Columbo";
-
-                    // $user->update();
-                    
-                    // Testing create new user                        
-                    // $photo = new Photo();
-
-                    // $photo->title = "Photo of lake";
-                    // $photo->size = 10;
-
-                    // $photo->create();
-
-                    // echo "mountains.jpg";
-
-                    // Testing update user last name
-                    // $user = User::find_user_by_id(5);
-                    // $user->last_name = "WILLIAMS";
-                    // $user->update();
-
-                    // Testing delete user
-                    // $user = User::find_user_by_id(8);
-                    // $user->delete();
-
-                    // Testing update user record on save
-                    // $user = User::find_user_by_id(9);
-                    // $user->first_name = "Charles";
-                    // $user->last_name = "Dupont";
-                    // $user->password = "123456";
-                    // $user->save();
-
-                    // Testing add user if username does not exist
-                    // $user = new User();
-                    // $user->username = "Samuel";
-                    // $user->save();
-
-                    //  Testing photo class
-                    // $photos = Photo::find_all();
-                    // foreach ($photos as $photo) {
-                    //     echo $photo->title;
-                    // }
-
-
-                    ?>
-
-
-
-                    <ol class="breadcrumb">
-                        <li>
-                            <i class="fa fa-dashboard"></i>  <a href="index.html">Dashboard</a>
-                        </li>
-                        <li class="active">
-                            <i class="fa fa-file"></i> Blank Page
-                        </li>
-                    </ol>
                 </div>
-            </div> <!-- /.row -->
-        </div> <!-- /.container-fluid -->
-    </div> <!-- /#page-wrapper -->
+            </div>
+            <div class="row">
+                <div class="col-lg-3 col-md-6">
+                    <div class="panel panel-primary">
+                        <div class="panel-heading">
+                            <div class="row">
+                                <div class="col-xs-3">
+                                    <i class="fa fa-users fa-5x"></i>
+                                </div>
+                                <div class="col-xs-9 text-right">
+                                    <div class="huge"><?php echo $session->count; ?></div>
+                                    <div>New Views</div>
+                                </div>
+                            </div>
+                        </div>
+                        <a href="#">
+                            <div class="panel-footer">
+                                <span class="pull-left">View Details</span>
+                                <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
+                                <div class="clearfix"></div>
+                            </div>
+                        </a>
+                    </div>
+                </div>
 
-  <?php include("includes/footer.php"); ?>
+                <div class="col-lg-3 col-md-6">
+                    <div class="panel panel-green">
+                        <div class="panel-heading">
+                            <div class="row">
+                                <div class="col-xs-3">
+                                    <i class="fa fa-photo fa-5x"></i>
+                                </div>
+                                <div class="col-xs-9 text-right">
+                                    <div class="huge"><?php echo Photo::count_all(); ?></div>
+                                    <div>Photos</div>
+                                </div>
+                            </div>
+                        </div>
+                        <a href="#">
+                            <div class="panel-footer">
+                                <span class="pull-left">Total Photos in Gallery</span>
+                                <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
+                                <div class="clearfix"></div>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+
+                <div class="col-lg-3 col-md-6">
+                    <div class="panel panel-yellow">
+                        <div class="panel-heading">
+                            <div class="row">
+                                <div class="col-xs-3">
+                                    <i class="fa fa-user fa-5x"></i>
+                                </div>
+                                <div class="col-xs-9 text-right">
+                                    <div class="huge">
+                                        <?php echo User::count_all(); ?>
+                                    </div>
+
+                                    <div>Users</div>
+                                </div>
+                            </div>
+                        </div>
+                        <a href="#">
+                            <div class="panel-footer">
+                                <span class="pull-left">Total Users</span>
+                                <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
+                                <div class="clearfix"></div>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+
+                <div class="col-lg-3 col-md-6">
+                    <div class="panel panel-red">
+                        <div class="panel-heading">
+                            <div class="row">
+                                <div class="col-xs-3">
+                                    <i class="fa fa-support fa-5x"></i>
+                                </div>
+                                <div class="col-xs-9 text-right">
+                                    <div class="huge"><?php echo Comment::count_all(); ?></div>
+                                    <div>Comments</div>
+                                </div>
+                            </div>
+                        </div>
+                        <a href="#">
+                            <div class="panel-footer">
+                                <span class="pull-left">Total Comments</span>
+                                <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
+                                <div class="clearfix"></div>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+            </div> <!--First Row-->
+
+            <div class="row">
+                <div id="piechart" style="width: 900px; height: 500px;"></div>
+            </div>
+
+            <?php include("includes/footer.php"); ?>
