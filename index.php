156184e35d5f2e48ec819ec08b2301acc6ec4234
Brent Mercer <hicx76@hotmail.com>
2019-09-12 09:29:30 -0600

    Front end photo gallery and

--- index.php
+++ index.php
@@ -1,15 +1,44 @@
 <?php include("includes/header.php"); ?>
 
-    <div class="row">
+<?php
 
+$photos = Photo::find_all();
+
+?>
+
+
+
+    <div class="row">
         <!-- Blog Entries Column -->
-        <div class="col-md-8">
+        <div class="col-md-12">
+            <div class="thumbnails row">
+
+            <?php
+            foreach ($photos as $photo) : ?>
+
+                <div class="col-xs-6 col-md-3 ">
+                    <a class="thumbnail" href="photo.php?id=<?php echo $photo->id; ?>">
+                        <img src="admin/<?php echo $photo->photo_path(); ?>" alt="" class="img-responsive home-page-photo">
+                    </a>
+                </div>
+                
+            <?php endforeach; ?>
+
+            </div>
         </div>
 
+
+
+
+
+
+
+
         <!-- Blog Sidebar Widgets Column -->
-        <div class="col-md-4">
-             <?php include("includes/sidebar.php"); ?>
-         </div>
+        <!-- <div class="col-md-4">
+             <?php // include("includes/sidebar.php"); ?>
+        </div> -->
+
 
     </div>
     <!-- /.row -->
--- photo.php
+++ photo.php
@@ -30,70 +30,64 @@ $comments = Comment::find_the_comments($photo->id);
 
 ?>
 
+<div class="row">
+    <div class="col-lg-12">
+        <!-- Blog Post -->
+        <!-- Title -->
+        <h1><?php echo $photo->title; ?></h1>
+        <!-- Author -->
+        <p class="lead">
+            by <a href="#">Brent Mercer</a>
+        </p>
+        <hr>
+        <!-- Date/Time -->
+        <p><span class="glyphicon glyphicon-time"></span> Posted on August 24, 2013 at 9:00 PM</p>
+        <hr>
+        <!-- Preview Image -->
+        <img class="img-responsive" src="admin/<?php echo $photo->photo_path(); ?>" alt="">
+        <hr>
+        <!-- Post Content -->
+        <p class="lead"><?php echo $photo->caption; ?></p>
+        <p><?php echo $photo->description; ?></p>
+        <hr>
 
-<div class="col-lg-8">
-    <!-- Blog Post -->
-    <!-- Title -->
-    <h1>Blog Post Title</h1>
-    <!-- Author -->
-    <p class="lead">
-        by <a href="#">Start Bootstrap</a>
-    </p>
-    <hr>
-    <!-- Date/Time -->
-    <p><span class="glyphicon glyphicon-time"></span> Posted on August 24, 2013 at 9:00 PM</p>
-    <hr>
-    <!-- Preview Image -->
-    <img class="img-responsive" src="http://placehold.it/900x300" alt="">
-    <hr>
-    <!-- Post Content -->
-    <p class="lead">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ducimus, vero, obcaecati, aut, error quam sapiente nemo saepe quibusdam sit excepturi nam quia corporis eligendi eos magni recusandae laborum minus inventore?</p>
-    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut, tenetur natus doloremque laborum quos iste ipsum rerum obcaecati impedit odit illo dolorum ab tempora nihil dicta earum fugiat. Temporibus, voluptatibus.</p>
-    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eos, doloribus, dolorem iusto blanditiis unde eius illum consequuntur neque dicta incidunt ullam ea hic porro optio ratione repellat perspiciatis. Enim, iure!</p>
-    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error, nostrum, aliquid, animi, ut quas placeat totam sunt tempora commodi nihil ullam alias modi dicta saepe minima ab quo voluptatem obcaecati?</p>
-    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Harum, dolor quis. Sunt, ut, explicabo, aliquam tenetur ratione tempore quidem voluptates cupiditate voluptas illo saepe quaerat numquam recusandae? Qui, necessitatibus, est!</p>
-    <hr>
-
-    <!-- Blog Comments -->
-    <!-- Comments Form -->
-    <div class="well">
-        <h4>Leave a Comment:</h4>
-        <form role="form" method="post">
-            <div class="form-group">
-                <label>Author</label>
-                <input type="text" name="author" class="form-control">
-            </div>
-            <div class="form-group">
-                <textarea name="body" class="form-control" rows="3"></textarea>
-            </div>
-            <button type="submit" name="submit" class="btn btn-primary">Submit</button>
-        </form>
-    </div>
-</div>
-
-
-<!-- Posted Comments -->             
-
-<?php foreach ($comments as $comment): ?>
-<!-- Comment -->
-<div class="media">
-    <a class="pull-left" href="#">
-        <img class="media-object" src="http://placehold.it/64x64" alt="">
-    </a>
-    <div class="media-body"><?php echo $comment->author; ?>
-        <h4 class="media-heading">Start Bootstrap</h4>
-        <?php echo $comment->body; ?>
-    </div>
-</div>
-<?php endforeach; ?>
-
-<!-- Blog Sidebar Widgets Column -->
-<div class="col-md-4">
-    <?php include("includes/sidebar.php"); ?>
-</div>
-
+        <!-- Blog Comments -->
+        <!-- Comments Form -->
+        <div class="well">
+            <h4>Leave a Comment:</h4>
+            <form role="form" method="post">
+                <div class="form-group">
+                    <label>Author</label>
+                    <input type="text" name="author" class="form-control">
+                </div>
+                <div class="form-group">
+                    <textarea name="body" class="form-control" rows="3"></textarea>
+                </div>
+                <button type="submit" name="submit" class="btn btn-primary">Submit</button>
+            </form>
         </div>
-        <!-- /.row -->
 
-        <?php include("includes/footer.php"); ?>
+
+    <!-- Posted Comments -->             
+    <?php foreach ($comments as $comment) : ?>
+        <!-- Comment -->
+        <div class="media">
+            <a class="pull-left" href="#">
+                <img class="media-object" src="http://placehold.it/64x64" alt="">
+            </a>
+            <div class="media-body"><?php echo $comment->author; ?>
+                <h4 class="media-heading">Start Bootstrap</h4>
+                <?php echo $comment->body; ?>
+            </div>
+        </div>
+    <?php endforeach; ?>
+
+    <!-- Blog Sidebar Widgets Column -->
+    <!-- <div class="col-md-4">
+        <?php // include("includes/sidebar.php"); ?>
+     -->
+    </div>
+</div>
+
+<?php include("includes/footer.php"); ?>
 
